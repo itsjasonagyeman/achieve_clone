1f2f8ee4bb15ce5c7a39761e2aa9fb1609247766
Jason Agyeman <agyemanjasonnhyira@gmail.com>
2025-01-07 12:59:25 +0000

    Add images and polished homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -16,14 +16,18 @@ class _HomepageState extends State<Homepage> {
         appBar: const CustomAppBar(),
         body: SingleChildScrollView(
           child: Padding(
-            padding:  const EdgeInsets.fromLTRB(15,8,15,8),
+            padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text('Your Investments', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),),
-                const SizedBox(height: 10,),
-                Container( //Investment balance
+                const Text(
+                  'Your Investments',
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  // Investment balance
                   height: 180,
                   width: double.infinity,
                   decoration: BoxDecoration(
@@ -33,8 +37,8 @@ class _HomepageState extends State<Homepage> {
                       colors: [Colors.blueAccent, Colors.lightBlue],
                       stops: [0.0, 0.8],
                       begin: Alignment.topRight,
-                      end: Alignment.bottomLeft
-                      )
+                      end: Alignment.bottomLeft,
+                    ),
                   ),
                   child: const Padding(
                     padding: EdgeInsets.all(25.0),
@@ -42,21 +46,40 @@ class _HomepageState extends State<Homepage> {
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Total Flexible', style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal, fontSize: 15),),
-                        Text('Investment Balance', style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal, fontSize: 15),),
+                        Text(
+                          'Total Flexible',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 15),
+                        ),
+                        Text(
+                          'Investment Balance',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 15),
+                        ),
                         Spacer(),
-                        Text('GHS 100.00', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),)
+                        Text(
+                          'GHS 100.00',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        )
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 10,),
-                Container( //Petra pensions
+                const SizedBox(height: 10),
+                Container(
+                  // Petra pensions
                   height: 180,
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.blueGrey,
-                    borderRadius: BorderRadius.circular(20)
+                    borderRadius: BorderRadius.circular(20),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(30),
@@ -65,62 +88,118 @@ class _HomepageState extends State<Homepage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Expanded(
-                          child: Column(                       
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Text('Petra Pensions', style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500, fontSize: 18)),
-                              const Text('Do you have a pension account with Petra? Tap here to link it now',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w400, fontSize: 15)),
-                              const SizedBox(height: 10,),
+                              const Text(
+                                'Petra Pensions',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18),
+                              ),
+                              const Text(
+                                'Do you have a pension account with Petra? Tap here to link it now',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 15),
+                              ),
+                              const SizedBox(height: 10),
                               Container(
                                 height: 40,
                                 width: 120,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(8),
-                                  color: Colors.white
+                                  color: Colors.white,
                                 ),
-                                child: const Center(child: Text('Start now', style: TextStyle(color: Colors.black,fontWeight: FontWeight.normal, fontSize: 15),)),
+                                child: const Center(
+                                  child: Text(
+                                    'Start now',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 15),
+                                  ),
+                                ),
                               )
                             ],
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 15,),
-                Container( //discover
+                const SizedBox(height: 15),
+                Container(
+                  // Discover section
                   height: 120,
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.purpleAccent,
                     borderRadius: BorderRadius.circular(20),
                     gradient: const LinearGradient(
-                      colors: [Color.fromARGB(255, 191, 86, 209), Color.fromARGB(255, 239, 150, 255)],
+                      colors: [
+                        Color.fromARGB(255, 191, 86, 209),
+                        Color.fromARGB(255, 239, 150, 255)
+                      ],
                       begin: Alignment.topLeft,
-                      end: Alignment.bottomRight
+                      end: Alignment.bottomRight,
                     ),
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.fromLTRB(120,20,40,10),
-                    child: Column(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text('Discover even more ways to build a financially-secured future', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400, fontSize: 15, height: 1),),
-                        SizedBox(height: 20,),
-                        Text('Discover >', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 15),)
+                        Image.asset(
+                          'lib/images/discover.png',
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 10),
+                        const Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Discover even more ways to build a financially-secured future',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 15,
+                                  height: 1.2,
+                                ),
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Discover >',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(height: 15,),
-                Container( //Manage your work
+                const SizedBox(height: 15),
+                Container(
+                  // Manage your work
                   height: 150,
                   width: double.infinity,
                   decoration: BoxDecoration(
                     color: const Color.fromARGB(255, 235, 192, 242),
-                    borderRadius: BorderRadius.circular(20)
+                    borderRadius: BorderRadius.circular(20),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(15.0),
@@ -128,17 +207,37 @@ class _HomepageState extends State<Homepage> {
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text('Manage your work', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),),
-                        const Text('View your payslips, make leave requests and do so much more with achievePayroll', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400, fontSize: 13),),
-                        const SizedBox(height: 10,),
+                        const Text(
+                          'Manage your work',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        const Text(
+                          'View your payslips, make leave requests and do so much more with achievePayroll',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 13),
+                        ),
+                        const SizedBox(height: 10),
                         Container(
                           height: 40,
                           width: 120,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(8),
-                            color: Colors.black
+                            color: Colors.black,
                           ),
-                          child: const Center(child: Text('Get Started', style: TextStyle(color: Colors.white,fontWeight: FontWeight.normal, fontSize: 15),)),
+                          child: const Center(
+                            child: Text(
+                              'Get Started',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 15),
+                            ),
+                          ),
                         )
                       ],
                     ),
