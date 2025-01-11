eedd349f99ebdd6b257eb49d152eb4f6132e16db
Jason Agyeman <agyemanjasonnhyira@gmail.com>
2025-01-11 20:57:11 +0000

    Finished Explore

--- lib/pages/Navigate from explore page/request_callback.dart
+++ lib/pages/Navigate from explore page/request_callback.dart
@@ -17,24 +17,49 @@ class _RequestCallbackState extends State<RequestCallback> {
       child: Container(
                     padding: const EdgeInsets.all(16),
                     height: 200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Custom Dialog",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text("Please be advised that one of our agents will reach to you within the next 24 hours"),
+                          const SizedBox(height: 10,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              GestureDetector(
+                                onTap: (){
+                                  Navigator.pop(context);
+                                },
+                                child: Container(
+                                  height: 40,
+                                  width: 120,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.blueGrey[200]
+                                  ),
+                                  child: const Center(child: Text('Cancel')),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: (){
+                                  Navigator.pop(context);
+                                },
+                                child: Container(
+                                  height: 40,
+                                  width: 120,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: const Color.fromARGB(255, 5, 33, 58)
+                                  ),
+                                  child: const Center(child: Text('Proceed', style: TextStyle(color: Colors.white),)),
+                                ),
+                              )
+                            ],
                           ),
-                        ),
-                        const Text("This is a custom dialog with a button."),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text("Close"),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
       ),
     );
